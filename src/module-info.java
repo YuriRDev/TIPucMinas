94375b3d75a6fb1b00c70eedc86921b936cb4599
Yuri <49888082+YuriRDev@users.noreply.github.com>
2022-08-23 16:27:16 -0300

    a

---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module pucminasti {
+}
---
+++ src/pucminasti/Main.java
@@ -0,0 +1,19 @@
+package pucminasti;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	// input 
+	Scanner input = new Scanner(System.in);
+	
+	// gettering + data
+	Double num1 = input.nextDouble();
+	Double num2 = input.nextDouble();
+	
+	// Print
+	System.out.println(num1 + num2);
+	}
+	
+}
